5b8beb22045fd9421c70d2b01d9649c380f17a86
Niklas Cathor <niklas.cathor@gmx.de>
2023-08-06 10:39:09 +0200

    move docs to doc/ on gh-pages

--- utils/prepare-docs-for-gh-pages.sh
+++ utils/prepare-docs-for-gh-pages.sh
@@ -8,10 +8,12 @@ set -x
 # verify that gh-pages directory does not exist
 [ ! -e gh-pages ]
 
+mkdir gh-pages
+
 # copy doc directory
-cp -a target/doc gh-pages
+cp -a target/doc gh-pages/
 
 # create index.html to redirect to the proper location
 # (source: https://dev.to/deciduously/prepare-your-rust-api-docs-for-github-pages-2n5i)
-echo '<meta http-equiv="refresh" content="0; url=usbh">' > gh-pages/index.html
+echo '<meta http-equiv="refresh" content="0; url=doc/usbh">' > gh-pages/index.html
 
