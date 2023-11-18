2c98526c0b72a5b6a774fd713295442d2e394232
nilclass <nilclass@users.noreply.github.com>
2023-11-18 12:10:52 +0000

    deploy: 68792edb121db65e724653cf61159ad9067f2e24

--- doc/src-files.js
+++ doc/src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "usbh":["",[["driver",[],["detector.rs","hub.rs","kbd.rs","log.rs"]]],["bus.rs","descriptor.rs","discovery.rs","driver.rs","enumeration.rs","enumerator.rs","lib.rs","transfer.rs","types.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
